9a103ee2fcf3dad13749ea8ac6d0e61852594722
Aki Peuralahti <aki.peuralahti@vitecsoftware.com>
2023-05-14 14:01:01 +0300

    Full slack openAI chat API integration
    Initial commit v1.0.0

---
+++ config.py
@@ -0,0 +1,8 @@
+SLACK_API_TOKEN = 'token here'
+SLACK_BOT_TOKEN = 'token here'
+VERIFICATION_TOKEN = 'token here'
+OPENAI_API_KEY = 'token here'
+OPENAI_ENGINE = 'gpt-3.5-turbo'
+ENCRYPTION_FILE = "encryption_key.key"
+ENCRYPTION_KEY = "CKEHyiTOLUwRFOm2zhDTfzTU-1xD6Ruy5lNS2SOIGdF="
+ENCRYPTION_SET  = True
